0e65200661c043c402117adfc9c0ef65ea19bf6f
matata <lzmopadoor@gmail.com>
2023-11-07 12:27:27 +0800

    Site updated: 2023-11-07 12:27:22

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FF9900",
+      "tmpSize": "16",
+      "cityColor": "CCCCCC",
+      "citySize": "16",
+      "aqiColor": "D9D9D9",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "15px 10px 10px 20px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "48f8337bc4f84a419699ca8eb6e14281"
+    }
+  }
